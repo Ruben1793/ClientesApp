fd84058c7294f70fce2852dd3a66dfaeb4add817
Ruben <ruben.ojed9a3@gmail.com>
2019-03-17 17:22:38 -0600

    Creacion del proyecto

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Bienvenido a Angular';
+  curso = 'Angular con Spring 5';
+  profesor = 'Andres Guzman';
+  alumno = 'Ruben Ojeda';
+}
