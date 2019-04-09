5d39fca74fb4b3314b6c7834315ec68be882a554
Ruben Ojeda <ruben.ojed9a3@gmail.com>
2019-04-08 21:38:07 -0500

    Creando clientes.json.ts con la lista de objetos

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { CLIENTES} from './clientes.json';
 
 @Component({
   selector: 'app-clientes',
@@ -7,17 +8,12 @@ import { Cliente } from './cliente';
 })
 export class ClientesComponent implements OnInit {
 
-  clientes: Cliente[] = [
-    {id: 1, nombre: 'Ruben', apellido: 'Ojeda', email: 'ruben.ojeda@gmail.com', createAt: '2017-12-11'},
-    {id: 1, nombre: 'Angel', apellido: 'Gonzalez', email: 'angel.caligra@gmail.com', createAt: '2017-11-12'},
-    {id: 1, nombre: 'Aaron', apellido: 'de Santiago', email: 'desatiago.aaron@gmail.com', createAt: '2017-10-11'},
-    {id: 1, nombre: 'Sofia', apellido: 'Terrazas', email: 'sofia.terrazas@gmail.com', createAt: '2017-11-11'},
-    {id: 1, nombre: 'Lizeth', apellido: 'Pacheco', email: 'lizeth.pacheco@gmail.com', createAt: '2017-10-13'}
-  ] ;
+  clientes: Cliente[] ;
 
   constructor() { }
 
   ngOnInit() {
+    this.clientes = CLIENTES;
   }
 
 }
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,9 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+    {id: 1, nombre: 'Ruben', apellido: 'Ojeda', email: 'ruben.ojeda@gmail.com', createAt: '2017-12-11'},
+    {id: 1, nombre: 'Angel', apellido: 'Gonzalez', email: 'angel.caligra@gmail.com', createAt: '2017-11-12'},
+    {id: 1, nombre: 'Aaron', apellido: 'de Santiago', email: 'desatiago.aaron@gmail.com', createAt: '2017-10-11'},
+    {id: 1, nombre: 'Sofia', apellido: 'Terrazas', email: 'sofia.terrazas@gmail.com', createAt: '2017-11-11'},
+    {id: 1, nombre: 'Lizeth', apellido: 'Pacheco', email: 'lizeth.pacheco@gmail.com', createAt: '2017-10-13'}
+  ] ;
