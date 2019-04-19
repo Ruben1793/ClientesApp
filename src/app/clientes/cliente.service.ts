85c6ddaab57de91319f8606c22911794cfce7cc0
Ruben Ojeda <ruben.ojed9a3@gmail.com>
2019-04-19 11:38:50 -0500

    Implementando servicio Angular con HttpClient

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -3,14 +3,19 @@ import { CLIENTES} from './clientes.json';
 import { Cliente } from './cliente';
 import { Observable } from 'rxjs';
 import { of } from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class ClienteService {
 
-  constructor() { }
+  private urlEndPoint = 'http://localhost:8080/api/clientes';
+
+  constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]> {
-    return of(CLIENTES);
+    // return of(CLIENTES);
+    return this.http.get<Cliente[]>(this.urlEndPoint);
    }
 
 }
