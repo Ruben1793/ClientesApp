15ab1ab3bc39c1686b85882c1b2f27c5ce16bb70
Ruben Ojeda <ruben.ojed9a3@gmail.com>
2019-05-01 16:11:15 -0500

    Creando component y vista de formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,14 @@ import { ClientesComponent } from './clientes/clientes.component';
 import { ClienteService } from './clientes/cliente.service';
 import { RouterModule, Routes} from '@angular/router';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { FormComponent } from './clientes/form.component';
+import { FormsModule } from '@angular/forms';
 
 const routes: Routes = [
   {path: '', redirectTo: '/clientes', pathMatch: 'full'},
   {path: 'directivas', component: DirectivaComponent},
-  {path: 'clientes', component: ClientesComponent}
+  {path: 'clientes', component: ClientesComponent},
+  {path: 'clientes/form', component: FormComponent}
 ];
 
 @NgModule({
@@ -23,11 +26,13 @@ const routes: Routes = [
     HeaderComponent,
     FooterComponent,
     ClientesComponent,
-    DirectivaComponent
+    DirectivaComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(routes)
   ],
   providers: [ClienteService],
