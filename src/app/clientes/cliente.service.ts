37b89e5ccb15f9cf028b238fb27ba107c289d422
Ruben Ojeda <ruben.ojed9a3@gmail.com>
2019-05-14 20:52:10 -0500

    Manejando error en Angular en obtener por id

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { CLIENTES} from './clientes.json';
 import { Cliente } from './cliente';
-import { Observable } from 'rxjs';
-import { of } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
+import swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class ClienteService {
@@ -13,7 +14,7 @@ export class ClienteService {
 
   private httpHeaders = new HttpHeaders({'Content-type': 'application/json'});
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   getClientes(): Observable<Cliente[]> {
     // return of(CLIENTES);
@@ -27,7 +28,14 @@ export class ClienteService {
    }
 
    getCliente(id: number): Observable<Cliente> {
-    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`);
+    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`).pipe(
+      catchError(e => {
+        this.router.navigate(['/clientes']);
+        console.log(e.error.mensaje);
+        swal('Error al editar', e.error.mensaje, 'error');
+        return throwError(e);
+      })
+    );
    }
 
    update(cliente: Cliente): Observable<Cliente> {
